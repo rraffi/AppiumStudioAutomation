da07d35f3add315d56c10a171627d9a4da2250a1
Riaz <testifact@gmail.com>
2018-04-09 17:59:25 -0400

    Updated UDID, Device name to Riaz.

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -11,38 +11,36 @@ import java.net.MalformedURLException;
 
 public class Runner {
 
-    String reportDirectory = "reports";
-    String reportFormat = "xml";
-    String testName = "Untitled";
-    IOSDriver<IOSElement> driver = null;
-    DesiredCapabilities dc = new DesiredCapabilities();
+        String reportDirectory = "reports";
+        String reportFormat = "xml";
+        String testName = "Untitled";
+        IOSDriver<IOSElement> driver = null;
+        DesiredCapabilities dc = new DesiredCapabilities();
 
-    @Before
-    public void setUp() throws MalformedURLException {
-        dc.setCapability("reportDirectory", reportDirectory);
-        dc.setCapability("reportFormat", reportFormat);
-        dc.setCapability("testName", testName);
-        dc.setCapability(MobileCapabilityType.UDID, "d7ee38fac7b2e2ceb592165c7a57f6aba1887952");
-        dc.setCapability("instrumentApp", true);    //DesiredCapability to instrument the app
-        driver = new IOSDriver<>(new URL("http://localhost:4723/wd/hub"), dc);
-    }
-
-    @Test
-    public void testUntitled() {
-        driver.executeScript("client:client.setDevice(\"ios_app:Riaz2\")");
-        driver.executeScript("client:client.launch(\"com.homedepot.homeservices.selling.qa.planning\", \"true\", \"true\")");
-        if(driver.findElement(By.xpath("//*[@text='Reset Password']"))!=null)
-        {
-            driver.findElement(By.xpath("//*[@text='Reset Password']")).click();
-            System.out.println("Found Element");
+        @Before
+        public void setUp () throws MalformedURLException {
+            dc.setCapability("reportDirectory", reportDirectory);
+            dc.setCapability("reportFormat", reportFormat);
+            dc.setCapability("testName", testName);
+            dc.setCapability(MobileCapabilityType.UDID, "e649b1ece17f0f7feefe5fe826ce6581f8ae407d");
+            dc.setCapability("instrumentApp", true);    //DesiredCapability to instrument the app
+            driver = new IOSDriver<>(new URL("http://localhost:4723/wd/hub"), dc);
         }
 
+        @Test
+        public void testUntitled () {
+            driver.executeScript("client:client.setDevice(\"ios_app:Riaz\")");
+            driver.executeScript("client:client.launch(\"com.homedepot.homeservices.selling.qa.planning\", \"true\", \"true\")");
+            if (driver.findElement(By.xpath("//*[@text='Reset Password']")) != null) {
+                driver.findElement(By.xpath("//*[@text='Reset Password']")).click();
+                System.out.println("Found Element");
+            }
 
-    }
 
-    @After
-    public void tearDown() {
-        driver.quit();
-    }
+        }
 
+        @After
+        public void tearDown () {
+            driver.quit();
+        }
 }
